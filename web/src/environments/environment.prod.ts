cbcb39f2caa32f566a13e63671e55cfc294051af
Alejandro de Jesús Ventura Martínez <aventura@e-gisnet.com>
2021-04-29 02:49:43 -0700

    api docker

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  URL_API: "http://localhost:8080/api/",
-  URL_API_AUTH: "http://localhost:8080/oauth/token",
-  HOST_PUBLIC: "http://localhost:4200/public/sucursal/",
+  URL_API: "http://gisnet-security-qtpbvzhgtn.dynamic-m.com:9093/api-gpc/",
+  URL_API_AUTH: "http://gisnet-security-qtpbvzhgtn.dynamic-m.com:9093/api-gpc/oauth/token",
   CLIENT_API_ID: "client_web",
+  HOST_PUBLIC: "http://gisnet-security-qtpbvzhgtn.dynamic-m.com:9093/public/sucursal/",
   CLIENT_API_KEY: "26JDsduwTkGmSyN9fqNvmX4CZbEjT7xK37Yk"
 };
